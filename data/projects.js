11b6530947f4f7c2b9f3ba202b9863bdf773f30c
Tai Nguyen Trong <trongtaind37@gmail.com>
2020-03-02 10:48:49 +0700

    Updates

--- data/projects.js
+++
@@ -1,8 +0,0 @@
-export default [
-  {
-    icon: '📝',
-    title: 'KeySpy-2019',
-    source: 'https://github.com/trongtai37/keyspy',
-    description: 'A Malware to log the keyboard and capture screenshot !',
-  }  
-]
